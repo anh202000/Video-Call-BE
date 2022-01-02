f404fd836bbd2c1ca340e750cdc58c59c8a5b277
Le Phan tuan Anh <anhroyal110@gmail.com>
2022-01-03 01:17:44 +0700

    fix basic Port to connection FE vs BE

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
-const express = require('express');
-const socket = require('socket.io');
+const express = require("express");
+const socket = require("socket.io");
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-const app = express;
+const app = express();
 
 const server = app.listen(PORT, () => {
-    console.log(`server is listening on port ${PORT}`);
-    console.log(`http://localhost:${PORT}`);
+  console.log(`server is listening on port ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
 });
-
-
 
 const io = socket(server, {
-    cors: {
-        origin: "*",
-        method: ['GET', 'POST']
-
-    }
+  cors: {
+    origin: "*",
+    method: ["GET", "POST"],
+  },
 });
 
-io.on('connection', (socket) => {
-    socket.emit('connection', null);
-    console.log('new user connected');
-    console.log(socket.io);
-})
+io.on("connection", (socket) => {
+  socket.emit("connection", null);
+  console.log("new user connected");
+  console.log(socket.io);
+});
